667a47bc350453b3f9296db4e85aa6c43dae2015
jupeeze <pokegames1578@gmail.com>
2024-12-11 08:03:52 +0900

    オブジェクト指向に基づいたリファクタリング

---
+++ Ground.cs
@@ -0,0 +1,40 @@
+﻿using DxLibDLL;
+using System.Runtime.InteropServices;
+
+internal class Ground
+{
+	// 画面サイズ
+	private const int SCREEN_X = 640, SCREEN_Y = 480;
+
+	// 画像のサイズ
+	private const int TILE_SIZE = 32;
+
+	// 画像の数
+	private const int TILE_COUNT = SCREEN_X / TILE_SIZE + 1;
+
+	// 画像のハンドル
+	private int _groundImage;
+
+	private int _slopeImage;
+
+	// 画像の移動
+	private int _diff;
+
+	public void Load() {
+		// PNG画像のメモリへの読みこみ
+		_groundImage = DX.LoadGraph(@"sprite/ground.png");
+	}
+
+	// 地面を描画する
+	public void Draw() {
+		int x = 0, y = 300;
+
+		// 差分を計算
+		_diff -= 2;
+		if (_diff <= -TILE_SIZE) _diff = 0;
+
+		for (int i = 0; i < TILE_COUNT; i++) {
+			DX.DrawGraph(x + i * TILE_SIZE + _diff, y, _groundImage, DX.TRUE);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -3,30 +3,33 @@
 using System;
 using DxLibDLL;
 
-internal class Program
+internal class Game
 {
-	// 画面サイズ
-	private static readonly int SCREEN_X = 640, SCREEN_Y = 480;
-
-	// 画像のサイズ
-	private static readonly int TILE_SIZE = 32;
-
-	// 画像の数
-	private static readonly int TILE_COUNT = SCREEN_X / TILE_SIZE + 1;
-
 	// タイマーの周期
-	private static readonly int TIMER_INTERVAL = 16;
+	private const int TIMER_INTERVAL = 16;
 
-	// 画像
-	private static int[] _grounds = new int[5];
-
-	// 画像の移動
-	private static int _diff = 0;
+	private Ground _ground;
 
 	// 画面更新の計測用タイマー
-	private static int _timer;
+	private int _timer;
 
-	private static void Init() {
+	public Game() {
+		_ground = new Ground();
+	}
+
+	public void Run() {
+		// 初期化処理
+		Init();
+		// 画像の読み込み
+		Load();
+		// メインループ
+		Loop();
+
+		// Dxlib の終了処理
+		DX.DxLib_End();
+	}
+
+	private void Init() {
 		// ウィンドウモードで起動するように設定
 		DX.ChangeWindowMode(DX.TRUE);
 		// Dxlib の初期化
@@ -38,20 +41,16 @@ internal class Program
 		_timer = DX.GetNowCount();
 	}
 
-	private static void Load() {
-		// PNG画像のメモリへの読みこみ
-		DX.LoadDivGraph(@"Sprite/Grounds.png", 5, 5, 1, 16, 16, _grounds);
+	public void Load() {
+		// 画像の読み込み
+		_ground.Load();
 	}
 
-	private static void Loop() {
+	private void Loop() {
 		// メインループ
 		while (DX.ProcessMessage() == 0) {
-			// 画面をクリア
-			DX.ClearDrawScreen();
 			// 画像を描画
 			Draw();
-			// 裏画面の内容を表画面に反映
-			DX.ScreenFlip();
 
 			// 周期 16ms で待機
 			_timer += TIMER_INTERVAL;
@@ -59,32 +58,22 @@ internal class Program
 		}
 	}
 
-	private static void Draw() {
-		// 差分を計算
-		_diff += 2;
-		if (_diff >= TILE_SIZE) {
-			_diff = 0;
-		}
+	private void Draw() {
+		// 画面をクリア
+		DX.ClearDrawScreen();
 
-		int x = 0, y = 100;
-		for (int i = 0; i < TILE_COUNT; i++) {
-			DrawEXGraph(x + i * TILE_SIZE - _diff, y, _grounds[0]);
-		}
+		// 地面を描画
+		_ground.Draw();
+
+		// 裏画面の内容を表画面に反映
+		DX.ScreenFlip();
 	}
+}
 
-	private static void DrawEXGraph(int x, int y, int GrHandle) {
-		DX.DrawExtendGraph(x, y, x + TILE_SIZE, y + TILE_SIZE, GrHandle, DX.TRUE);
-	}
-
+internal class Program
+{
 	private static void Main(string[] args) {
-		// 初期化処理
-		Init();
-		// 画像の読み込み
-		Load();
-		// メインループ
-		Loop();
-
-		// Dxlib の終了処理
-		DX.DxLib_End();
+		Game game = new Game();
+		game.Run();
 	}
 }
