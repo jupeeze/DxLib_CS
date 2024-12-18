56c1647782ba5fc62c6c376a535f2539ff2caf39
jupeeze <pokegames1578@gmail.com>
2024-12-18 19:23:29 +0900

    Summonクラスの作成・インスタンス化する

--- Enemy.cs
+++ Enemy.cs
@@ -1,29 +1,117 @@
 ﻿using DxLibDLL;
+using System.Collections.Generic;
 
 internal static class Enemy
 {
-	private static readonly int SIZE_X = 100, SIZE_Y = 100;
+	private static List<Summon> _summons = new List<Summon>();
 
+	private static readonly int SIZE_X = 100, SIZE_Y = 100;
+	private static readonly int ATTACK_INTERVAL = 120;
+
+	// 画像の差し替え
 	private static int _imageNum = 0;
 
-	private static int[] _enemyImages;
+	// 攻撃のタイミング
+	private static int _attackTiming = 0;
+
+	// 画像のハンドル
+	private static List<int[]> _enemyImages;
+
+	// 今攻撃しているか
+	private static bool _isAttacking = false;
 
 	public static void Load() {
-		LoadSprite("sprite/Undead executioner/idle2.png", 4, 2);
+		_enemyImages = new List<int[]>();
+
+		if (_enemyImages.Count == 0) {
+			LoadSprite("sprite/Undead executioner/idle2.png", 4, 2);
+			LoadSprite("sprite/Undead executioner/summon.png", 4, 2);
+		}
 	}
 
 	private static void LoadSprite(string filePath, int divX, int divY) {
 		int spriteCount = divX * divY;
-		_enemyImages = new int[spriteCount];
-		DX.LoadDivGraph(filePath, spriteCount, divX, divY, SIZE_X, SIZE_Y, _enemyImages);
+		int[] sprites = new int[spriteCount];
+		DX.LoadDivGraph(filePath, spriteCount, divX, divY, SIZE_X, SIZE_Y, sprites);
+		_enemyImages.Add(sprites);
+	}
+
+	public static void Update() {
+		Attack();
 	}
 
 	public static void Draw() {
-		DX.DrawExtendGraph(Game.SCREEN_X - SIZE_X, Game.GROUND_POS - (2 * SIZE_Y), Game.SCREEN_X - (3 * SIZE_X), Game.GROUND_POS, _enemyImages[_imageNum], DX.TRUE);
+		int imageIndex = _isAttacking ? 1 : 0;
+
+		DX.DrawExtendGraph(Game.SCREEN_X - SIZE_X, Game.GROUND_POS - (2 * SIZE_Y), Game.SCREEN_X - (3 * SIZE_X), Game.GROUND_POS, _enemyImages[imageIndex][_imageNum], DX.TRUE);
+
+		foreach (var summon in _summons)
+			if (summon.IsActive)
+				summon.Draw();
+
+		uint _black = DX.GetColor(0, 0, 0);
+		uint _white = DX.GetColor(255, 255, 255);
+
+		if (_summons.Count > 0) {
+			for (int i = 0; i < _summons.Count; i++) {
+				DX.DrawString(50, 100 + (i * 50),
+						$"{_summons[i].IsActive}", _black, _white);
+				DX.DrawString(100, 100 + (i * 50),
+						$"{_summons[i].IsMoving}", _black, _white);
+			}
+		}
+	}
+
+	public static void Animator() {
+		_imageNum = 0;
 	}
 
 	public static void Animation() {
 		_imageNum++;
-		_imageNum %= 8;
+
+		int maxFrame = _isAttacking ? 5 : 8;
+
+		_imageNum %= maxFrame;
+
+		if (_isAttacking && _imageNum == 0) {
+			_isAttacking = false;
+		}
+
+		foreach (var summon in _summons) {
+			if (summon.IsActive) {
+				summon.Animation();
+			}
+		}
+	}
+
+	public static void Attack() {
+		_attackTiming++;
+		if (_attackTiming % ATTACK_INTERVAL == 0) {
+			_isAttacking = true;
+			Animator();
+
+			if (Summonable()) {
+				var summon = new Summon();
+				summon.Load();
+				_summons.Add(summon);
+			}
+		}
+	}
+
+	public static bool Summonable() {
+		foreach (Summon summon in _summons) {
+			if (summon.IsActive && !summon.IsMoving) {
+				summon.Attack();
+				return false;
+			}
+		}
+
+		var inactiveSummon = _summons.Find(s => !s.IsActive);
+		if (inactiveSummon != null) {
+			inactiveSummon.Load();
+			return false;
+		}
+
+		return true;
 	}
 }
---
+++ Summon.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using DxLibDLL;
+
+internal class Summon
+{
+	private const int SIZE_X = 50, SIZE_Y = 50;
+	private const int MOVE_SPEED = 30;
+	private const int APPEAR_FRAMES = 6;
+	private const int ACTIVE_FRAMES = 4;
+
+	private int _posX, _posY;
+	private int _currentFrame;
+	private double _targetRad;
+	private List<int[]> _enemyImages = new List<int[]>();
+
+	private enum SummonState
+	{ Appearing, Idle, Moving, Inactive }
+
+	private SummonState _state = SummonState.Appearing;
+
+	public bool IsActive => _state != SummonState.Inactive;
+	public bool IsMoving => _state == SummonState.Moving;
+
+	public void Load() {
+		_state = SummonState.Appearing;
+		_currentFrame = 0;
+
+		_posX = Game.SCREEN_X - SIZE_X;
+		_posY = Game.GROUND_POS - (2 * SIZE_Y);
+
+		if (_enemyImages.Count == 0) {
+			LoadSprite("sprite/Undead executioner/summonAppear.png", 3, 2);
+			LoadSprite("sprite/Undead executioner/summonIdle.png", 4, 1);
+		}
+	}
+
+	private void LoadSprite(string filePath, int divX, int divY) {
+		int spriteCount = divX * divY;
+		var sprites = new int[spriteCount];
+		DX.LoadDivGraph(filePath, spriteCount, divX, divY, SIZE_X, SIZE_Y, sprites);
+		_enemyImages.Add(sprites);
+	}
+
+	public void Draw() {
+		int imageIndex = (_state == SummonState.Appearing) ? 0 : 1;
+		DX.DrawExtendGraph(_posX, _posY, _posX - (2 * SIZE_X), _posY + (2 * SIZE_Y), _enemyImages[imageIndex][_currentFrame], DX.TRUE);
+	}
+
+	public void Animation() {
+		_currentFrame++;
+		switch (_state) {
+			case SummonState.Appearing:
+				if ((_currentFrame %= APPEAR_FRAMES) == 0) {
+					_state = SummonState.Idle;
+				}
+				break;
+
+			case SummonState.Idle:
+			case SummonState.Moving:
+				_currentFrame %= ACTIVE_FRAMES;
+				break;
+		}
+	}
+
+	public void Attack() {
+		_targetRad = Math.Atan2(-(Player._posY - Game.GROUND_POS), Game.SCREEN_X - SIZE_X);
+		_state = SummonState.Moving;
+	}
+
+	public void Move() {
+		_posX -= (int)(MOVE_SPEED * Math.Cos(_targetRad));
+		_posY -= (int)(MOVE_SPEED * Math.Sin(_targetRad));
+
+		if (_posX <= -SIZE_X) {
+			_state = SummonState.Inactive;
+		}
+	}
+}
