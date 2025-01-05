a8ffea864304fa378bb9b043cfbcd46a9082e2e9
jupeeze <pokegames1578@gmail.com>
2025-01-05 21:19:10 +0900

    プレイヤーの二段ジャンプの実装

--- Code/Player.cs
+++ Code/Player.cs
@@ -4,15 +4,16 @@ using System;
 internal static class Player
 {
 	private static readonly int SIZE_X = 50, SIZE_Y = 37;
-	private static readonly int JUMP_FORCE = -20;
+	private static readonly int JUMP_FORCE = -15;
 	private static readonly int GRAVITY_INCREMENT = 1;
 
 	private static int _imagePosY2 = Game.GROUND_POS;
 
 	private static int _gravity = 0;
+	private static int _availableJumps = 2;
+	private static bool _isJumpKeyPressed = false;
 
 	private static int _imageNum = 0;
-
 	private static int[] _playerImages;
 
 	public static readonly int ImagePosX1 = SIZE_X;
@@ -43,6 +44,7 @@ internal static class Player
 			if (_state == PlayerState.Running) {
 				_gravity = 0;
 				_imagePosY2 = Game.GROUND_POS;
+				_availableJumps = 2;
 			}
 		}
 	}
@@ -72,10 +74,20 @@ internal static class Player
 	}
 
 	private static void HandleInput() {
-		if (DX.GetMouseInput() == DX.MOUSE_INPUT_LEFT && State == PlayerState.Running) {
-			State = PlayerState.Jumping;
-			_gravity = JUMP_FORCE;
+		// 左クリックが押された瞬間を検知
+		bool isMouseLeftClicked = DX.GetMouseInput() == DX.MOUSE_INPUT_LEFT;
+
+		if (isMouseLeftClicked && !_isJumpKeyPressed) {
+			// ジャンプ処理
+			if (_availableJumps > 0) {
+				State = PlayerState.Jumping;
+				_gravity = JUMP_FORCE;
+				_availableJumps--;
+			}
 		}
+
+		// 現在のボタン状態を更新
+		_isJumpKeyPressed = isMouseLeftClicked;
 	}
 
 	private static void UpdatePosition() {
@@ -99,8 +111,7 @@ internal static class Player
 	public static void Draw() {
 		DX.DrawExtendGraph(ImagePosX1, ImagePosY1, ImagePosX2, ImagePosY2, GetCurrentImage(), DX.TRUE);
 
-		uint Cr = DX.GetColor(0, 0, 255);
-		DX.DrawBox(HitboxPosX1, HitboxPosY1, HitboxPosX2, HitboxPosY2, Cr, DX.FALSE);
+		DX.DrawBox(HitboxPosX1, HitboxPosY1, HitboxPosX2, HitboxPosY2, Game.BLUE_COLOR, DX.FALSE);
 	}
 
 	private static int GetCurrentImage() {
